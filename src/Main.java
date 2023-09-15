f7acd65fece9eacffb5fe3cc05c2aea3ba1ce341
小茗同学 <2398290381@qq.com>
2023-09-15 15:48:35 +0000

    ran

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("请输入一个整数");
+        int num=sc.nextInt();
+        System.out.println("您输入的整数为："+num);
+    }
+    }
